dc3faf45c651aa78e6390344de30fe2e560a1d1a
Jason Stein <jasonstein@college.harvard.edu>
2015-04-22 15:25:20 -0400

    attempt at insert function

--- final.c
+++ final.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-struct node 
+typedef struct node 
 {
     bool is_word;
     struct node* children [26];
-};
+}
+node;
 
+node root = {false,{NULL}};
 
+int insert(char* word)
+{
+    int length = strlen(word);
+    node* crawl = &root;
+    for(int i = 0; i < length; i++)
+    {
+        int index = word[i] - 'a';
+        if (crawl->children[index] != NULL)
+        {
+            crawl = crawl->children[index];      
+        }
+        else 
+        {
+            crawl->children[index] = malloc(sizeof(node));
+            crawl = crawl->children[index];
+        }
+        if (i == length - 1)
+        {
+            crawl->is_word = true;
+        }
+    };
+}
