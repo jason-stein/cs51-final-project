56340b88a86417e251dca71f7e10f70b095cecb1
Jason Stein <jasonstein@college.harvard.edu>
2015-05-01 15:32:03 -0400

    added timers

--- common.c
+++ common.c
@@ -7,6 +7,8 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <assert.h>
+#include <sys/resource.h>
+#include <sys/time.h>
 
 #include "common.h"
 
@@ -469,4 +471,21 @@ void print_finalists (list_node* front)
     }
 }
 
-
+/**
+ * Returns number of seconds between b and a.
+ */
+double calculate(const struct rusage* b, const struct rusage* a)
+{
+    if (b == NULL || a == NULL)
+    {
+        return 0.0;
+    }
+    else
+    {
+        return ((((a->ru_utime.tv_sec * 1000000 + a->ru_utime.tv_usec) -
+                 (b->ru_utime.tv_sec * 1000000 + b->ru_utime.tv_usec)) +
+                ((a->ru_stime.tv_sec * 1000000 + a->ru_stime.tv_usec) -
+                 (b->ru_stime.tv_sec * 1000000 + b->ru_stime.tv_usec)))
+                / 1000000.0);
+    }
+}
